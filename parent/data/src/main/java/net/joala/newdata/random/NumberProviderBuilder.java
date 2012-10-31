69ad924ffd7578be0b1db43d2173c0c8f52dc44b
Mark Michaelis <mark.michaelis@coremedia.com>
2012-10-31 19:00:57 +0100

    joala, #24 Make seeds for randomgenerators configurable
    Javadoc.

--- parent/data/src/main/java/net/joala/newdata/random/NumberProviderBuilder.java
+++ parent/data/src/main/java/net/joala/newdata/random/NumberProviderBuilder.java
@@ -3,9 +3,18 @@ package net.joala.newdata.random;
 import javax.inject.Provider;
 
 /**
+ * <p>
+ *   Builder interface for number providers.
+ * </p>
  * @since 10/23/12
  */
 public interface NumberProviderBuilder<T extends Number, P extends Provider<T>>
         extends Provider<T>, FluentNumberRange<T> {
+  /**
+   * <p>
+   * Create a number provider of the given type.
+   * </p>
+   * @return prepared and configured number provider
+   */
   P build();
 }
