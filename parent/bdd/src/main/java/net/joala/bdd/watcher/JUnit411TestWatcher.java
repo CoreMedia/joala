d0b2976029c563e439114bb5253c5ee162f30e01
Mark Michaelis <mark.michaelis@coremedia.com>
2012-11-28 00:34:20 +0100

    joala, #29 Move to GPL for Licensing
    Adding GPL to all files.

--- parent/bdd/src/main/java/net/joala/bdd/watcher/JUnit411TestWatcher.java
+++ parent/bdd/src/main/java/net/joala/bdd/watcher/JUnit411TestWatcher.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 CoreMedia AG
+ *
+ * This file is part of Joala.
+ *
+ * Joala is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Joala is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Copy of org.junit.rules.TestWatcher from not yet released 4.11 JUnit Release
 // TODO: Remove as soon as updated to JUnit 4.11
 package net.joala.bdd.watcher;
